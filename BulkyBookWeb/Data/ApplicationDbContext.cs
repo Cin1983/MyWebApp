baa7789fed71f9d37292036f3c4dfd96f5b9bf5e
Maria Groeizaam <cinnymaria@gmail.com>
2022-08-30 11:27:39 +0200

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data;
+
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //Connect the table
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+}
+   
+
